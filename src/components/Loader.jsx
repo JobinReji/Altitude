7aeca10020194ea809abe827430b0dfd45ec03d9
JobinReji <jobinreji14@gmail.com>
2025-05-28 15:38:41 +0530

    Loader update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,17 @@
+import loader from "../assets/Loader.png";
+
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-full w-full">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#5224b5]"></div>
+    <div className="flex flex-col justify-center items-center h-full w-full gap-2">
+      {/* Spinner with image */}
+      <div className="animate-spin rounded-full h-12 w-12">
+        <img src={loader} alt="Loading..." className="h-full w-full" />
+      </div>
+
+      {/* Animated "Loading..." text */}
+      <div className="text-[#5224b5] font-medium">
+        Loading<span className="typing"></span>
+      </div>
     </div>
   );
 };
